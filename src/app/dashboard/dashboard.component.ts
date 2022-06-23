f7bff5d30255a4d595811fbf1361d18460439e5d
e536367 <david.lienberger@sbb.ch>
2022-06-23 15:40:17 +0200

    add dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  track: number = 3;
+  stations: number = 8;
 
   constructor() { }
 
